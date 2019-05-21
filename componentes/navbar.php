8f69076ba042d5790fb385c98d4f81cd77482223
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2019-05-21 16:42:22 -0400

    INIT: initial commit for semillero public page

---
+++ componentes/navbar.php
@@ -0,0 +1,19 @@
+<div class="classynav">
+    <ul>
+        <li><a href="index.html">INICIO</a></li>
+        <li><a id="scrollDown" href="#quienessomos">El Semillero</a></li>
+        <li><a href="#">Escuelas</a>
+            <ul class="dropdown">
+                <li><a href="#canchas">Tenis</a></li>
+                <li><a href="#canchas">Futbol</a></li>
+                <!--<li><a href="elements.html">Horarios</a></li>-->
+            </ul>
+        </li>
+        <!--<li><a href="#noticias">Noticias</a></li>-->
+        <li><a href="#contacto">Contacto</a></li>
+    </ul>
+
+    <!-- Call Button -->
+    <!--<a href="#" class="fitness-btn menu-btn ml-30">Reservas: +56975162995</a>-->
+
+</div>
